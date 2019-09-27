273c6d8d077354903f719511d6bac3ec308cac01
seeris <sergei.udris@gmail.com>
2019-09-27 10:27:26 +0300

    build

---
+++ geoserver/scripts/jetty-cors.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# download Jetty servlets needed for CORS
+
+# http://central.maven.org/maven2/org/eclipse/jetty/jetty-servlets/9.4.12.v20180830/jetty-servlets-9.4.12.v20180830.jar
