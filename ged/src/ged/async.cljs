2a8e845ef3067f2d08f644cb6f70eaec368a9caa
seeris <sergei.udris@gmail.com>
2019-10-05 12:23:27 +0300

    async

--- ged/src/ged/async.cljs
+++ ged/src/ged/async.cljs
@@ -1,3 +1,18 @@
 (ns ged.async
-  (:require [clojure.core.async :as async]))
+  (:require [clojure.core.async :as async :refer [chan <! >! close!]])
+  (:require-macros [cljs.core.async.macros :refer [go]]))
 
+#_(chan)
+
+#_(chan 10)
+
+#_(let [c (chan)]
+    (close! c))
+
+#_(let [c (chan 10)]
+    (>! c "hello")
+    (assert (= "hello" (<! c) ))
+    (close! c)
+    )
+
+#_a/>!
--- ged/src/ged/http.cljs
+++ ged/src/ged/http.cljs
@@ -3,5 +3,12 @@
             [re-frame.core :as rf]
             [day8.re-frame.tracing :refer-macros [fn-traced defn-traced]]))
 
+; https://developer.mozilla.org/en-US/docs/Web/API/AbortController
+; https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
 
-
+(rf/reg-event-fx
+ :http
+ (fn-traced
+  [{:keys [db]} [_ ea]]
+  (let []
+    {})))
